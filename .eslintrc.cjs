27149bbebb4ccd95ccbd368b3209d686a3d26f1e
MetroHege <113589548+MetroHege@users.noreply.github.com>
2024-03-18 13:42:58 +0200

    project start

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended"
+    ],
+    ignorePatterns: ["dist", ".eslintrc.cjs"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["react-refresh", "prettier"],
+    extends: ["plugin:prettier/recommended"],
+    rules: {
+        "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+        "require-jsdoc": 0,
+        "prettier/prettier": [
+            "error",
+            {
+                endOfLine: "auto"
+            }
+        ]
+    }
+};
