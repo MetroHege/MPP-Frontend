575c2055f8ed09b6e8bc7b7037b4c0acb2641b33
MetroHege <113589548+MetroHege@users.noreply.github.com>
2024-04-04 20:33:35 +0300

    desperate tries

--- src/views/Single.tsx
+++ src/views/Single.tsx
@@ -1,12 +1,19 @@
 import { PostListingsResponse, User } from "mpp-api-types";
 import { Carousel } from "react-responsive-carousel";
 import { NavigateFunction, useLocation, useNavigate } from "react-router-dom";
+import { useUser } from "../hooks/UserHooks";
+import useListing from "../hooks/ListingHooks";
+import ListingForm from "../components/ListingForm";
+import { useState } from "react";
 
 const Single = () => {
     const { state } = useLocation();
     const navigate: NavigateFunction = useNavigate();
     const item: PostListingsResponse = state;
     const userItem: User = state.user;
+    const { user } = useUser();
+    const [showForm, setShowForm] = useState(false);
+    const { deleteListing } = useListing();
 
     return (
         <div className="flex">
@@ -28,7 +35,35 @@ const Single = () => {
                         {parseInt(item.price)} €
                     </p>{" "}
                 </div>
-                <h2 className="mb-4 text-2xl">Lisätiedot</h2>
+                <div className="flex justify-between items-center">
+                    <h2 className=" text-2xl">Lisätiedot</h2>
+                    {/* {user?.id === item.user.id && (
+                        <div className=" flex space-x-4 items-center">
+                            <button
+                                className="w-1/2 p-2 bg-yellow-gradient font-bold mb-2 rounded hover:brightness-75"
+                                type="submit"
+                                onClick={() => setShowForm(true)}
+                            >
+                                Muokkaa
+                            </button>
+                            <ListingForm showForm={showForm} setShowForm={setShowForm} />
+                            <button
+                                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                                onClick={async () => {
+                                    if (user?.id === item.user.id) {
+                                        await deleteListing(
+                                            item.id,
+                                            (user as User & { token: string }).token
+                                        );
+                                        // You might want to refresh the page or redirect the user after the listing is deleted
+                                    }
+                                }}
+                            >
+                                Poista
+                            </button>
+                        </div>
+                    )} */}
+                </div>
                 <hr className="w-full mt-2 mb-3 border-gray-300" />
                 <p className="mb-10">{item.description}</p>
                 <div>
@@ -45,12 +80,10 @@ const Single = () => {
                     <p className="text-2xl">Kategoria:</p>
                 </div>
                 <div className="w-1/2">
-                    <p className="text-lg">
-                        {userItem.firstName} {userItem.lastName}
-                    </p>
+                    <p className="text-2xl">{userItem.username}</p>
                     <p className="text-2xl">{item.type === "buy" ? "Ostetaan" : "Myydään"}</p>
                     <p className="text-2xl">{new Date(item.time).toLocaleDateString("fi-FI")}</p>
-                    <p className="text-2xl">{item.user.city}</p>
+                    <p className="text-2xl">{userItem.city}</p>
                     <p className="text-2xl">{item.quality}</p>
                     <p className="text-2xl">
                         {typeof item.category === "number" ? item.category : item.category.title}
