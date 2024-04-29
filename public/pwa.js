2e71f1f559ff5d698de7a5a56eeff9911695e268
MetroHege <113589548+MetroHege@users.noreply.github.com>
2024-04-29 12:35:03 +0300

    PWA

---
+++ public/pwa.js
@@ -0,0 +1,15 @@
+(async () => {
+  if ('serviceWorker' in navigator) {
+    try {
+      const worker = await navigator.serviceWorker.register('./sw.js');
+      console.log('Service Worker Registered');
+      console.log(worker);
+    } catch (e) {
+      if (e instanceof DOMException) {
+        console.log('SW registration failed');
+      } else if (e instanceof Error) {
+        console.log(e.message);
+      }
+    }
+  }
+})();
